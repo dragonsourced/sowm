f14937deba7fa7b96b9b9ff8fe0b3e0aebf7c2e9
dragonsourced <dragonsourced@protonmail.com>
2020-01-11 23:23:34 -0500

    Added RSS and Unicode character shortcuts.

--- config.h
+++ config.h
@@ -16,6 +16,8 @@ const char* music[]   = {"music.sh",        0};
 const char* fman[]    = {"fman.sh",         0};
 const char* quit[]    = {"killall", "sowm", 0};
 const char* layout[]  = {"nextlayout.sh",   0};
+const char* unicode[] = {"unicode.sh",      0};
+const char* rss[]     = {"rss.sh",          0};
 
 static struct key keys[] = {
     {MOD,           XK_q,   win_kill,   {0}},
@@ -30,7 +32,9 @@ static struct key keys[] = {
     {MOD, XK_h,      run, {.com = hphones}},
     {MOD, XK_m,      run, {.com = music}},
     {MOD, XK_a,      run, {.com = fman}},
+    {MOD, XK_r,      run, {.com = rss}},
     {MOD, XK_s,      run, {.com = scrot}},
+    {MOD, XK_u,      run, {.com = unicode}},
     {MOD, XK_space,  run, {.com = layout}},
     {MOD, XK_Return, run, {.com = term}},
 
